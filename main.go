969ec1e920aceb4ce443a0d3a843a3d7e8cc388f
artem <isakowww@yandex.com>
2025-03-26 10:44:39 +0300

    Исправлены текстовые сообщения

--- main.go
+++ main.go
@@ -7,10 +7,10 @@ import (
 
 func main() {
     var input string
-    fmt.Print("Введите целое число: ")
+    fmt.Print("Введите данные: ")
     _, err := fmt.Scanln(&input)
     if err != nil {
         log.Fatal(err)
     }
-    fmt.Printf("Вы ввели число:%s\n", input)
+    fmt.Printf("Вы ввели следующие данные: %s\n", input)
 }
